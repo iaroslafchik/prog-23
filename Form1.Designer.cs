cc19c9532da037c403d3b7f04bbc646554dbb92f
Yorick <y.kovanev@mail.ru>
2024-04-15 10:23:58 +0600

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace lab_prog_3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.DisplayedCells;
+            this.dataGridView1.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.DisplayedCells;
+            this.dataGridView1.ColumnHeadersVisible = false;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowHeadersVisible = false;
+            this.dataGridView1.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.DisableResizing;
+            this.dataGridView1.Size = new System.Drawing.Size(283, 268);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
+            // 
+            // button3
+            // 
+            this.button3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button3.Location = new System.Drawing.Point(545, 257);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(116, 23);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button4.Location = new System.Drawing.Point(545, 286);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(116, 23);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.numericUpDown1.Location = new System.Drawing.Point(545, 12);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(116, 20);
+            this.numericUpDown1.TabIndex = 5;
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(301, 55);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(225, 225);
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(673, 321);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab_prog_3
+{
+    public partial class Form1 : Form
+    {
+        int oldNodeCount = 4;
+        int nodeCount = 5;
+
+        public Form1()
+        {
+            InitializeComponent();
+            numericUpDown1.Value = nodeCount;
+            dataGridView1.RowCount = nodeCount;
+            dataGridView1.ColumnCount = nodeCount;
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            button3.Text = string.Format("Заполнить ({0})", dataGridView1.Rows.Count * dataGridView1.Columns.Count);
+            button4.Text = string.Format("Удалить ({0})", dataGridView1.Rows.Count * dataGridView1.Columns.Count);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            var rand = new Random();
+
+            foreach (DataGridViewRow row in dataGridView1.Rows)
+            {
+                foreach (DataGridViewCell cell in row.Cells)
+                {
+                    if (cell.RowIndex == cell.ColumnIndex)
+                        cell.Value = 0;
+
+                    if (cell.RowIndex < cell.ColumnIndex)
+                        cell.Value = rand.Next(2);
+
+                    if (cell.RowIndex > cell.ColumnIndex)
+                        cell.Value = dataGridView1[cell.RowIndex, cell.ColumnIndex].Value;
+                }
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            dataGridView1.Rows.Clear();
+            dataGridView1.Columns.Clear();
+            Refresh();
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            oldNodeCount = nodeCount;
+            nodeCount = (int)numericUpDown1.Value;
+            
+            if (nodeCount > oldNodeCount)
+            {
+                DataGridViewColumn column = new DataGridViewColumn();
+                column.CellTemplate = new DataGridViewTextBoxCell();
+                dataGridView1.Columns.Add(column);
+                dataGridView1.Rows.Add(new DataGridViewRow());
+            }
+            else if (nodeCount < oldNodeCount)
+            {
+                dataGridView1.Columns.Remove(new DataGridViewColumn());
+                dataGridView1.Rows.Remove(new DataGridViewRow());
+            }
+
+            pictureBox1.Refresh();
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            int j = e.RowIndex;
+            int i = e.ColumnIndex;
+            if (i == j)
+                return;
+            int value = (int)(dataGridView1[i, j].Value);
+            value = (value + 1) % 2;
+            dataGridView1[i, j].Value = value;
+            dataGridView1[j, i].Value = value;
+        }
+
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics gr = e.Graphics;
+
+            Pen circle = new Pen(Color.Black);
+            Pen edge = new Pen(Color.Blue)
+            {
+                Width = 4
+            };
+            int nodeRadius = 16;
+            double nodeStep = 2 * Math.PI / nodeCount;
+            Font nodeFont = new Font(FontFamily.GenericSansSerif, 10);
+
+            int circleRadius = 200;
+
+            Point pictureBoxCenter = new Point()
+            {
+                X = pictureBox1.Width / 2,
+                Y = pictureBox1.Height / 2
+            };
+
+            for (int i = 0; i < nodeCount; i++)
+            {
+                Size nodeSize = new Size()
+                {
+                    Width = nodeRadius,
+                    Height = nodeRadius
+                };
+                Point nodePosition = new Point()
+                {
+                    X = (int)(circleRadius / 2 * Math.Cos(i * nodeStep) + pictureBoxCenter.X - nodeSize.Width / 2),
+                    Y = (int)(circleRadius / 2 * Math.Sin(i * nodeStep) + pictureBoxCenter.Y - nodeSize.Height / 2)
+                };
+                Rectangle nodeFrame = new Rectangle(nodePosition, nodeSize);
+                gr.FillEllipse(Brushes.Red, nodeFrame);
+                gr.DrawString((i + 1).ToString(), nodeFont, Brushes.Black, nodePosition);
+            }
+        }
+    }
+}
